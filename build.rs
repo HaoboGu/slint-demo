47d2167a057f4ec6e7ffd1a3e27d962b540ad001
Haobo Gu <haobogu@outlook.com>
2024-06-17 15:20:35 +0800

    feat: demo that compile
    Signed-off-by: Haobo Gu <haobogu@outlook.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,41 @@
+use std::{env, fs::File, io::Write, path::PathBuf};
+
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    )
+    .unwrap();
+
+    // Put `memory.x` in our output directory and ensure it's
+    // on the linker search path.
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out.display());
+
+    // By default, Cargo will re-run a build script whenever
+    // any file in the project changes. By specifying `memory.x`
+    // here, we ensure the build script is only re-run when
+    // `memory.x` is changed.
+    println!("cargo:rerun-if-changed=memory.x");
+
+    // Specify linker arguments.
+
+    // `--nmagic` is required if memory section addresses are not aligned to 0x10000,
+    // for example the FLASH and RAM sections in your `memory.x`.
+    // See https://github.com/rust-embedded/cortex-m-quickstart/pull/95
+    println!("cargo:rustc-link-arg=--nmagic");
+
+    // Set the linker script to the one provided by cortex-m-rt.
+    println!("cargo:rustc-link-arg=-Tlink.x");
+
+    // Set the extra linker script from defmt
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
+
+    // Use flip-link overflow check: https://github.com/knurling-rs/flip-link
+    println!("cargo:rustc-linker=flip-link");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,81 @@
+#![no_std]
+#![no_main]
+
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use embedded_alloc::Heap;
+use slint::platform::software_renderer::MinimalSoftwareWindow;
+use embassy_stm32 as _;
+extern crate alloc;
+use alloc::{boxed::Box, rc::Rc};
+use defmt_rtt as _;
+use panic_probe as _;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+const HEAP_SIZE: usize = 200 * 1024;
+static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
+#[global_allocator]
+static ALLOCATOR: Heap = Heap::empty();
+
+struct MyPlatform {
+    window: Rc<MinimalSoftwareWindow>,
+}
+
+impl slint::platform::Platform for MyPlatform {
+    fn create_window_adapter(
+        &self,
+    ) -> Result<Rc<dyn slint::platform::WindowAdapter>, slint::PlatformError> {
+        Ok(self.window.clone())
+    }
+
+    fn duration_since_start(&self) -> core::time::Duration {
+        core::time::Duration::from_millis(embassy_time::Instant::now().as_millis())
+    }
+
+    fn debug_log(&self, arguments: core::fmt::Arguments) {
+        defmt::info!("{}", defmt::Debug2Format(&arguments));
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    // Initialize the heap allocator, peripheral devices and other things.
+    let p = embassy_stm32::init(Default::default());
+    
+    // Initialize heap
+    unsafe { ALLOCATOR.init(core::ptr::addr_of_mut!(HEAP) as usize, HEAP_SIZE) }
+
+    // Initialize a window (we'll need it later).
+    let window = MinimalSoftwareWindow::new(Default::default());
+    slint::platform::set_platform(Box::new(MyPlatform {
+        window: window.clone(),
+    }))
+    .unwrap();
+
+    // Setup the UI.
+    let ui = AppWindow::new().unwrap();
+    // // ... setup callback and properties on `ui` ...
+
+    // // Make sure the window covers our entire screen.
+    window.set_size(slint::PhysicalSize::new(320, 240));
+    // let mut buffer_provider = DrawBuffer {
+    //     display,
+    //     buffer: &mut [Rgb565Pixel(0); 240],
+    // };
+    // ... start event loop (see later) ...
+    loop {
+        slint::platform::update_timers_and_animations();
+        // window.draw_if_needed(|renderer| {
+        //     renderer.render_by_line(&mut buffer_provider);
+        // });
+
+        if !window.has_active_animations() {
+            if let Some(duration) = slint::platform::duration_until_next_timer_update() {
+                Timer::after(Duration::from_millis(duration.as_millis() as u64)).await;
+                continue;
+            }
         }
-    });
-
-    ui.run()
+        Timer::after(Duration::from_millis(10)).await;
+    }
 }
